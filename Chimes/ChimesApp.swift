a63b5833b724727ffda6da81c81a0e4a445531b1
Mitchell Kember <mk12360@gmail.com>
2025-06-03 18:51:30 -0400

    Initial Commit

---
+++ Chimes/ChimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChimesApp.swift
+//  Chimes
+//
+//  Created by Mitchell Kember on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ChimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
